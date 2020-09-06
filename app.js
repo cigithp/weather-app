cbdc04ae3789b35c8f8af48f6998daac8a02215d
Cigith <cigithpillai@gmail.com>
2020-09-06 12:46:55 -0700

    destructuring objects

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const forecast = require('./utils/forecast')
 const log = console.log
 const location = process.argv[2]
 //object destructuring
-forecast(location, (error, {location, weather_description : description, curr_temp : current, feels_like_temp : feels}) => {
+forecast(location, (error, {location, weather_description : description, curr_temp : current, feels_like_temp : feels} = {}) => {
     if(error) {
         return log(error)
     }
--- utils/forecast.js
+++ utils/forecast.js
@@ -3,17 +3,17 @@ const encodeUrl = require('encodeurl')
 
 const forecast = (location, callback) => {
     const url = "http://api.weatherstack.com/current?access_key=c020501743bb6828ff05a14281e1280f&query="+ encodeUrl(location) + "&units=f"
-    request(url, {json: true}, (error, response) => {
+    request(url, {json: true}, (error, { body } = {}) => {
         if(error) {
             callback('Unable to connect to weather service', undefined)
-        } else if(response && response.body.error) {
+        } else if(body.error) {
             callback('Unable to find the location', undefined)
         } else {
             callback(undefined, {
-                weather_description: response.body.current.weather_descriptions[0],
-                curr_temp: response.body.current.temperature,
-                feels_like_temp: response.body.current.feelslike,
-                location: response.body.location.name + ',' + response.body.location.region + ',' + response.body.location.country
+                weather_description: body.current.weather_descriptions[0],
+                curr_temp: body.current.temperature,
+                feels_like_temp: body.current.feelslike,
+                location: body.location.name + ',' + body.location.region + ',' + body.location.country
             })
         }
     })
